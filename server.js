34c6137fa9130d72cc2a12a05e2e6c044bdcc43f
unknown <gabeclementej@gmail.com>
2022-06-24 17:59:17 -0300

    feat port server listen three

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var http = require('http')
     ,app = require('./config/express');
 
-var port_number = server.listen(process.env.PORT || 3000);
+var port_number = server.listen(process.env.PORT || 3000, LOCAL_ADDRESS='0.0.0.0');
 app.listen(port_number);
+
+const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env
+server.listen(PORT, LOCAL_ADDRESS, () => {
+  const address = server.address();
+  console.log('server listening at', address);
+});
     
 http.createServer(app).listen(3000, function() {
     console.log('Servidor escutando na porta: ' + this.address().port);
