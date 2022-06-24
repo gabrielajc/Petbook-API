83f8334ef7ce9d57b0a5b0e3d4576fad3e3a825a
unknown <gabeclementej@gmail.com>
2022-06-24 20:22:52 -0300

    fix por address

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http')
-    ,app = require('./config/express');
-    
-http.createServer(app).listen(process.env.PORT || 3000, LOCAL_ADDRESS='0.0.0.0', function() {
-    console.log('Servidor escutando na porta: ' + this.address().port);
-});
+    , app = require('./config/express');
 
+http.createServer(app).listen(process.env.PORT || 3000, LOCAL_ADDRESS = '0.0.0.0', function () {
+    console.log('Servidor escutando na porta: ' + this.address().port);
+
+});
