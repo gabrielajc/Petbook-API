586b9f2f0bc435671dee52cf0a75f8a517d58ef8
unknown <gabeclementej@gmail.com>
2022-06-24 17:54:31 -0300

    feat port server listen

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http = require('http')
     ,app = require('./config/express');
+
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
     
 http.createServer(app).listen(3000, function() {
     console.log('Servidor escutando na porta: ' + this.address().port);
